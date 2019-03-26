7a41b7fafa1407b2e360730f32d2f2999fe92b48
Kevin Olsson <kevin.olsson@comeon.com>
2019-03-26 19:09:05 +0000

    initial layout with sass

--- src/components/CasinoDashboard.js
+++ src/components/CasinoDashboard.js
@@ -3,12 +3,18 @@ import CasinoGames from './CasinoGames';
 import PlayerDashboard from './PlayerDashboard';
 
 class CasinoDashboard extends React.Component {
+  state = {
+    filter: '',
+    games: {},
+  }
+
   render() {
     const { player } = this.props;
+    const { filter, games } = this.state;
     return (
       <div className="casino-dashboard">
-        <CasinoGames />
-        <PlayerDashboard player={player} />
+        <CasinoGames games={games} />
+        <PlayerDashboard player={player} filter={filter} />
       </div>
     );
   }
--- src/components/PlayerDashboard.js
+++ src/components/PlayerDashboard.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 class PlayerDashboard extends React.Component {
   render() {
+    const { player } = this.props;
     return (
       <div className="player-dashboard">
-        <h1>Hello {this.props.player.details.name}</h1>
+        <h1>
+          Hello
+          {player.details.name}
+        </h1>
       </div>
     );
   }
