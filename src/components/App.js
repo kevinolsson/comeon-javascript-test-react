9dfc5681c9222e0c2ebeaaa26c85bee7f62a511b
Kevin Olsson <kevin.olsson@comeon.com>
2019-03-26 16:44:37 +0000

    header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './Header';
 import Login from './Login';
 import Casino from './Casino';
 import NotFound from './NotFound';
@@ -7,13 +8,16 @@ import NotFound from './NotFound';
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route path="/casino" component={Casino} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
+      <React.Fragment>
+        <Header />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/casino" component={Casino} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../images/logo.svg';
+
+const Header = () => (
+  <div className="header__wrapper">
+    <div className="header">
+      <img src={logo} alt="" />
+    </div>
+  </div>
+);
+
+export default Header;
